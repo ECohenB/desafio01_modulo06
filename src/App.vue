5a811a3a4afd75cf86a2d91b70742953763eb628
Emariana Cohen <emariana.cohen@gmail.com>
2024-12-26 04:15:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="carnet">
+    <aside>
+      <div>
+        <span class="cedula-titulo">CEDULA DE IDENTIDAD</span>
+        <img width="40" src="/public/assets/chile_bandera.png" alt="Bandera de Chile" />
+      </div>
+      <img src="/public/assets/foto_elon.jpg" alt="Foto del titular" />
+      <p>RUN {{ run }}</p>
+    </aside>
+
+    <main>
+      <header>
+        <p>REPUBLICA DE CHILE</p>
+        <span>SERVICIO DE REGISTRO CIVIL E IDENTIFICACIÓN</span>
+      </header>
+      <section>
+        <article>
+          <header>APELLIDOS</header>
+          <span>{{ apellidos }}</span>
+        </article>
+        <article>
+          <header>NOMBRES</header>
+          <span>{{ nombres }}</span>
+        </article>
+
+        <div class="grid-2-colums">
+          <article>
+            <header>NACIONALIDAD</header>
+            <span>{{ nacionalidad }}</span>
+          </article>
+          <article>
+            <header>SEXO</header>
+            <span>{{ sexo }}</span>
+          </article>
+        </div>
+        <div class="grid-2-colums">
+          <article>
+            <header>FECHA DE NACIMIENTO</header>
+            <span>{{ fechaNacimiento }}</span>
+          </article>
+          <article>
+            <header>NÙMERO DOCUMENTO</header>
+            <span>{{ numeroDocumento }}</span>
+          </article>
+        </div>
+        <div class="grid-2-colums">
+          <article>
+            <header>FECHA DE EMISIÒN</header>
+            <span>{{ fechaEmision }}</span>
+          </article>
+          <article>
+            <header>FECHA DE VENCIMIENTO</header>
+            <span>{{ fechaVencimiento }}</span>
+          </article>
+        </div>
+        <article>
+          <header>FIRMA DEL TITULAR</header>
+          <span>{{ firma }}</span>
+        </article>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      run: "22.222.222-2",
+      apellidos: "MUSK",
+      nombres: "ELON REEVE",
+      nacionalidad: "SUDAFRICANO",
+      sexo: "M",
+      fechaNacimiento: "28 JUN 1971",
+      numeroDocumento: "222.222.222",
+      fechaEmision: "01 ENE 2025",
+      fechaVencimiento: "01 ENE 2030",
+      firma: "X Æ A-12",
+    };
+  },
+};
+</script>
+
+<style scoped>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  font-size: 10px;
+  font-family: sans-serif;
+}
+
+.carnet {
+  width: 400px;
+  height: 230px;
+  background: #c8deed;
+  border-radius: 20px;
+  display: grid;
+  grid-template-columns: 30% 1fr;
+  padding: 15px;
+  box-shadow: 0px 0px 10px 2px black;
+}
+.cedula-titulo {
+  font-size: 11px;
+  text-align: left;
+  font-weight: normal;
+}
+aside div {
+  display: flex;
+}
+
+aside > img {
+  margin-top: 15px;
+  width: 100%;
+}
+
+aside p {
+  text-align: center;
+  font-size: 12px;
+  font-weight: bold;
+}
+
+main {
+  padding: 0px 14px;
+  text-align: left;
+}
+
+main article {
+  margin: 0;
+  text-align: left;
+ 
+}
+
+main article header {
+  font-weight: bold;
+  color: #0235a4;
+  font-size: 10px;
+  line-height: 1.1;
+}
+main header {
+  margin: 0;
+}
+
+main header p {
+  font-size: 20px;
+  margin: 0;
+  line-height: 1;
+  text-align: left;
+  font-weight: bold;
+  color: #0235a4;
+}
+
+main header span {
+  font-size: 8px;
+  margin: 0;
+  color: #399be1;
+}
+
+main section {
+  margin: 0;
+}
+
+main section article span {
+  font-size: 13px;
+  font-weight: bold;
+}
+
+main section .grid-2-colums {
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+</style>
